964cd91a504e310bb4b926fabbea986bacdb5b59
coder-hoq <enamulhoquesifat1@gmail.com>
2025-11-12 23:15:08 +0600

    sifat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Runtime Terror Sifat");
+  res.send("Runtime Terror Sifat hoq");
 });
 
 app.listen(port, () => {
